cf2b3cf00ec2251e315110661aae451bdaba0d4d
Somali Conflict Analysis Group <somaliconflictanalysis@gmail.com>
2020-02-03 18:45:24 -0500

    Add fatalities by type

--- NEP Analysis/nep_analysis.R
+++ NEP Analysis/nep_analysis.R
@@ -25,3 +25,23 @@ print(sum(sum_1998$fatalities))
 ## Calculating sum of fatalities in 2015 (second highest year)
 sum_2015 <- data_nep[data_nep$year == 2015,]
 print(sum(sum_2015$fatalities))
+
+## Calculating total fatalities by conflict type
+conflict_type <- aggregate(data_nep$fatalities, by = list(type = data_nep$event_type), FUN = sum)
+conflict_type <- as.data.frame(conflict_type)
+names(conflict_type) <- c("type", "fatalities")
+
+## Graphing fatalities by conflict type in NEP
+ggplot(conflict_type, aes(type, fatalities)) + 
+  geom_col(fill = "#045a8d")+
+  theme(axis.text.x = element_text(size=15, face = "bold"),
+        axis.text.y = element_text(size=15),
+        plot.title = element_text(size = 40, face = "bold"),
+        plot.caption = element_text(size = 12.5)) + 
+  labs(x= NULL, y = NULL, title = "Fatalities in North Eastern Province (Kenya) by conflict type",
+       caption = "Data as of January 18, 2020
+       Source: ACLED, Somali Conflict Analysis Group") +
+  geom_text(aes(label = fatalities), vjust = -0.5, size = 7.5) +
+  ggsave(filename = "NEP Analysis/Figures/TypeFatalities.png", last_plot(),
+         width = 20, height = 10, dpi = 400)
+
